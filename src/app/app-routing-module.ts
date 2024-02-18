23f33b62e43cbc5e719533333f4edee332a7a79c
cpandares <pandaresocesar@gmail.com>
2024-02-18 09:35:16 -0400

    add routes

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { Routes,RouterModule } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomePageComponent,
+    },
+    {
+        path:'about',
+        component: AboutPageComponent,
+    },
+    
+    {
+        path:'**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+]
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(routes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
